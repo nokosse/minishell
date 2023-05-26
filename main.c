ea1f228e6e81a6f05b85c80c1c1cb0edd55dcf2b
nokosse <visouthivong.kevin@gmail.com>
2023-05-26 14:53:06 +0200

    Revert "Revert "test main""
    This reverts commit 595b01915c7a79011f03e32cd298f25310de8a7c.

--- main.c
+++ main.c
@@ -6,8 +6,14 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 15:30:19 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/05/26 13:41:34 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/05/26 14:38:23 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-potit changement
+#include "minishell.h"
+
+int main (void)
+{
+    ft_printf("minishell> ");
+    return (0);
+}
