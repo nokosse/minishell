2a4b50cf8a0578bf1dc9f1b6caef86e2ff548bc1
nokosse <visouthivong.kevin@gmail.com>
2023-06-02 13:57:20 +0200

    temporary fix for executor

--- src/executor.c
+++ src/executor.c
@@ -6,11 +6,41 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 17:35:52 by operez            #+#    #+#             */
-/*   Updated: 2023/05/30 12:26:08 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/06/02 13:23:50 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+// int	check_path(char **tokens, char **envp)
+// {
+// 	int		i;
+// 	char	*path;
+// 	char	**split;
+// 	char	*command;
+
+// 	i = 0;
+// 		path = ft_strdup(getenv("PATH"));
+// 	printf("path = %s\n", path);
+// 	(void) envp;
+// 	split = ft_split(path, ':');
+// 	while (split[i])
+// 	{
+// 		split[i] = ft_strjoin(split[i], "/");
+// 		command = ft_strjoin(split[i], tokens[0]);			//gere que la premiere commande pour l instant
+// 		ft_printf("command = %s\n", command);
+// 		if (access(command, X_OK) == 0)
+// 		{
+// 			tokens[0] = command;
+// 			ft_printf("command valid\n");
+// 			return (1);
+// 		}
+// 		i++;
+// 	}
+// 	ft_printf("Command not found\n");
+// 	return (0);
+// }
+
 
 int	check_path(char **tokens, char **envp)
 {
@@ -20,13 +50,15 @@ int	check_path(char **tokens, char **envp)
 	char	*command;
 
 	i = 0;
-	path = getenv("PATH");
+		path = ft_strdup(getenv("PATH"));
+	printf("path = %s\n", path);
 	(void) envp;
 	split = ft_split(path, ':');
 	while (split[i])
 	{
 		split[i] = ft_strjoin(split[i], "/");
 		command = ft_strjoin(split[i], tokens[0]);			//gere que la premiere commande pour l instant
+		command = ft_strtrim(command, " ");
 		ft_printf("command = %s\n", command);
 		if (access(command, X_OK) == 0)
 		{
@@ -58,7 +90,9 @@ void	executor(char **tokens, char **envp)
 		else
 		{
 			if (execve(tokens[0], tokens, NULL) == -1)
+			{
 				perror("error");
+			}
 		}
 	}
 }
