0f20072306c7a0f94a5dca96c604ee86cb9d2307
nokosse <visouthivong.kevin@gmail.com>
2023-06-13 11:02:59 +0200

    adaptation avec ft_strcmp plus simple

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:26:17 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/06/09 13:51:54 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/06/13 11:01:55 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,46 @@
 // unset
 // env
 // exit
-
-// TODO : trim cmd before strncmp
 int check_builtins(char *cmd)
 {
-	// printf("cmd in check builtins = %s\n", cmd);
-	if (ft_strncmp(cmd, "echo", 4) == 0)
+	if (ft_strcmp(cmd, "echo") == 0)
 		return (1);
-	if (ft_strncmp(cmd, "cd", 2) == 0) // command not found in check_path
+	if (ft_strcmp(cmd, "cd") == 0)
 		return (1);
-	if (ft_strncmp(cmd, "export", 6) == 0) // command not found in check_path
+	if (ft_strcmp(cmd, "export") == 0)
 		return (1);
-	if (ft_strncmp(cmd, "unset", 5) == 0) // command not found in check_path
+	if (ft_strcmp(cmd, "unset") == 0)
 		return (1);
-	if (ft_strncmp(cmd, "env", 3) == 0)
+	if (ft_strcmp(cmd, "env") == 0)
 		return (1);
-	if (ft_strncmp(cmd, "exit", 4) == 0)
+	if (ft_strcmp(cmd, "exit") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "pwd") == 0)
 		return (1);
 	return (0);
 }
 
+// This function will execute the right builtin function depending on the cmd
 void	exec_builtins(char **cmd, char **envp)
 {
-	printf("exec_builtins : cmd[0] = %s\n", cmd[0]);
-	// printf("executing builtins\n");
-	// printf("cmd[0] = %s\n", cmd[0]);
-	// cmd[0] = ft_strrchr(cmd[0], '/') + 1;
-	// printf("cmd[0] = %s\n", cmd[0]);
 	(void) envp;
-	if (ft_strncmp(cmd[0], "echo", 4) == 0)
+	if (ft_strcmp(cmd[0], "echo") == 0)
 		builtin_echo(cmd);
-	// if (ft_strcmp(cmd[0], "cd") == 0)
+	if (ft_strcmp(cmd[0], "cd") == 0)
+		printf("Normalement on execute builtin_cd ici <<<<<<<<<<<\n");
 	// 	builtin_cd(cmd, envp);
-	// if (ft_strcmp(cmd[0], "export") == 0)
+	if (ft_strcmp(cmd[0], "pwd") == 0)
+		printf("Normalement on execute builtin_pwd ici <<<<<<<<<<<\n");
+	if (ft_strcmp(cmd[0], "export") == 0)
+		printf("Normalement on execute builtin_export ici <<<<<<<<<<<\n");
 	// 	builtin_export(cmd, envp);
-	// if (ft_strcmp(cmd[0], "unset") == 0)
+	if (ft_strcmp(cmd[0], "unset") == 0)
+		printf("Normalement on execute builtin_unset ici <<<<<<<<<<<\n");
 	// 	builtin_unset(cmd, envp);
-	// if (ft_strcmp(cmd[0], "env") == 0)
+	if (ft_strcmp(cmd[0], "env") == 0)
+		printf("Normalement on execute builtin_env ici <<<<<<<<<<<\n");
 	// 	builtin_env(cmd, envp);
-	// if (ft_strcmp(cmd[0], "exit") == 0)
+	if (ft_strcmp(cmd[0], "exit") == 0)
+		printf("Normalement on execute builtin_exit ici <<<<<<<<<<<\n");
 	// 	builtin_exit(cmd, envp);
 }
