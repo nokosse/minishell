0bf9470bd8e3cdd272c5a6ea5f99f4539fac6a68
nokosse <visouthivong.kevin@gmail.com>
2023-12-19 17:18:12 +0100

    added comment

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:29:05 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/12/19 17:15:24 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/12/19 17:18:06 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 # include "minishell.h"
 # include "structure.h"
 
-// Lexer part
+/* LEXER PART */
 void	lexer(t_mini *shell);
-int		count_tokens(char *cmdl);
-char	*get_token(char *cmdl, int *j);
 void	init_lex(t_mini *shell);
 void	assign_word(t_mini *shell);
 void	assign_token(t_mini *shell);
 void	free_lex(t_mini *shell);
+int		count_tokens(char *cmdl);
+char	*get_token(char *cmdl, int *j);
 
-// Parser part
+/* PARSER PART */
 
-// Expander part
+/* EXPANDER PART */
 
 #endif
