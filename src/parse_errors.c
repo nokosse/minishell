fa9419f7f0802aab264bfdfebb276ec33b5ad7ad
nokosse <visouthivong.kevin@gmail.com>
2023-12-26 12:14:41 +0100

    disabled parsing to fix lexer

--- src/parse_errors.c
+++ src/parse_errors.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 17:51:12 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/12/25 17:53:28 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/12/26 12:14:20 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 /*
 This function will handle the input errors.
-
 */
 void	parse_error(t_mini *shell)
 {
-	
+	(void)shell;
 }
