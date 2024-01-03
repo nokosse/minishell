3c3597ee37ae9a14ef00ffd20cb1b0c9c071604f
nokosse <visouthivong.kevin@gmail.com>
2024-01-03 14:46:07 +0100

    normed

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 17:21:56 by kvisouth          #+#    #+#             */
-/*   Updated: 2024/01/03 14:08:41 by kvisouth         ###   ########.fr       */
+/*   Updated: 2024/01/03 14:45:40 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,6 +124,40 @@ int	get_cmdlines_in_nodes(t_mini *shell)
 }
 
 /*
+A long IF statement that I moved in this function to make the code shorter
+for norm issues.
+It checks if the previous token is NOT a Redirection and the current token
+is a WORD.
+*/
+int	check_prev_token(t_lex *lex_t, int prev_token)
+{
+	if (prev_token != RIGHT1 && prev_token != RIGHT2
+		&& prev_token != LEFT1 && prev_token != LEFT2
+		&& lex_t->token == WORD)
+		return (1);
+	return (0);
+}
+
+/*
+Same, just init variables for norm issues.
+*/
+void	init_i_j_prev(int *i, int *j, int *prev_token)
+{
+	*i = 0;
+	*j = 0;
+	*prev_token = -1;
+}
+
+/*
+Same, just init variables for norm issues.
+*/
+void	init_cmd_lex(t_mini *shell, t_cmd **cmd_t, t_lex **lex_t)
+{
+	*cmd_t = shell->cmd;
+	*lex_t = shell->lex;
+}
+
+/*
 This function will allocate the right number or arrays 'cmd' needs.
 */
 int	allocate_cmd_arrays(t_mini *shell)
@@ -134,16 +168,11 @@ int	allocate_cmd_arrays(t_mini *shell)
 	int		j;
 	int		prev_token;
 
-	i = 0;
-	j = 0;
-	cmd_t = shell->cmd;
-	lex_t = shell->lex;
-	prev_token = -1;
+	init_i_j_prev(&i, &j, &prev_token);
+	init_cmd_lex(shell, &cmd_t, &lex_t);
 	while (i < shell->nb_tokens)
 	{
-		if ((lex_t->token == WORD) && (prev_token != RIGHT1)
-			&& (prev_token != RIGHT2) && (prev_token != LEFT1)
-			&& (prev_token != LEFT2))
+		if (check_prev_token(lex_t, prev_token))
 			j++;
 		if (lex_t->token == PIPE || i == shell->nb_tokens - 1)
 		{
@@ -172,18 +201,13 @@ int	get_cmd_in_nodes(t_mini *shell)
 	int		j;
 	int		prev_token;
 
-	i = 0;
-	j = 0;
-	cmd_t = shell->cmd;
-	lex_t = shell->lex;
-	prev_token = -1;
+	init_i_j_prev(&i, &j, &prev_token);
+	init_cmd_lex(shell, &cmd_t, &lex_t);
 	if (!allocate_cmd_arrays(shell))
 		return (0);
 	while (i < shell->nb_tokens)
 	{
-		if (prev_token != RIGHT1 && prev_token != RIGHT2
-			&& prev_token != LEFT1 && prev_token != LEFT2
-			&& lex_t->token == WORD)
+		if (check_prev_token(lex_t, prev_token))
 			cmd_t->cmd[j++] = lex_t->word;
 		if (lex_t->token == PIPE || i == shell->nb_tokens - 1)
 		{
